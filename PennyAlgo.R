482a82e1cfb412da9e583141348495676cbd6161
Luke <lkavenagh@gmail.com>
2013-11-09 12:39:21 -0500

    .

--- PennyAlgo.R
+++ PennyAlgo.R
@@ -1,6 +1,10 @@
 # Install the quantmod package to get quote data
 #install.packages("quantmod")
+# Install RPostgreSQL to interact with PostgreSQL database
+#install.packages("RPostgreSQL")
+
 library("quantmod")
+library("RPostgreSQL")
 
 # Contants
 MAX_STOCKS <- 25
@@ -8,26 +12,50 @@ MAX_PRICE <- 0.05
 FREQ = 60
 
 # Not sure how to access the local csv file, there must be a better way than this...
-setwd("R:\\Documents\\GitHub\\penny-algo-R")
+#setwd("R:\\Documents\\GitHub\\penny-algo-R")
+setwd("C:\\Users\\Luke\\Documents\\GitHub\\penny-algo-R")
 
 # Read the universe of penny stocks
 # Need to figure out how/where to just grab this from the www
-universe <- read.csv("PennyList.csv",nrows=100,header=F,col.names="ticker")
+universe <- read.csv("PennyList.csv",header=F,col.names="ticker")
+
+# Connect to DB
+drv=dbDriver("PostgreSQL")
+con=dbConnect(drv, dbname="PennyAlgo", user="postgres", password="pianobookkittenmonster")
+
+# Insert universe into Security table
+for (i in universe$ticker) {
+  rs=dbGetQuery(con,paste("SELECT Ticker FROM Security WHERE Ticker = '",i,"'",sep=""))
+  if (length(rs)==0) {
+    dbSendQuery(con,paste("INSERT INTO Security (Ticker, Active) VALUES ('",i,"',TRUE)",sep=""))
+  }
+}
+
+# Mark as inactive if not in file
+rs = dbGetQuery(con, "SELECT Ticker FROM Security");
+
+inactive = rs$ticker[!(rs$ticker %in% universe$ticker)]
+
+for (i in inactive) {
+  dbSendQuery(con,paste("UPDATE Security SET Active = FALSE WHERE Ticker = '",i,"'",sep=""))
+}
 
 # Get current prices
-universe["price"] <- array(NA,2)
-for (i in 1:dim(universe)[1]) {
-  tmp <- getQuote(toString(universe["ticker"][i,1]))[2]
-  universe["price"][i,1] <- tmp
+universe = dbGetQuery(con,"SELECT Ticker FROM Security WHERE Active = TRUE")
+universe$price <- array(NA,length(universe$ticker))
+for (i in universe$ticker) {
+  tmp <- getQuote(i)
+  universe$price[universe$ticker==i] <- tmp$Last
 }
-universe = universe[universe["price"] < MAX_PRICE,]
+
+universe = subset(universe, price < MAX_PRICE)
 
 # We will rebalance the whole portfolio (sell all stocks and enter new ones) every FREQ days
 # On other days, we will just exit positions that have made 50% since entering
 
 # Which stocks should we invest in (if this is a rebalance day)
-selection <- sample(1:dim(universe)[1],MAX_STOCKS)
+selection <- sample(1:nrow(universe),min(nrow(universe),MAX_STOCKS))
 
-universe <- universe[selection,]
+universeSelection <- universe[selection,]
 
-print(universe)
+print(universeSelection)
